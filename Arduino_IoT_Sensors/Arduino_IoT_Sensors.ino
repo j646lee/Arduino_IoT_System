67f0fcc372bbfe5cb83c3c616cafb462cd8fae8c
Jiwon Lee <jiwonlee646@gmail.com>
2025-08-29 13:34:17 +0700

    Update Arduino_IoT_Sensors.ino

--- Arduino_IoT_Sensors/Arduino_IoT_Sensors.ino
+++ Arduino_IoT_Sensors/Arduino_IoT_Sensors.ino
@@ -1,21 +1,21 @@
 #define USE_POT_FOR_TEMP 0   
 
-const int PIN_TMP  = A0; #Temperature Sensor
-const int PIN_SOIL = A1; #Soil Sensor     
-const int PIN_LDR  = A2; #Light Sensor    
-const int PIN_POT  = A3; #Potentiometer     
-const int PIN_LED  = 13; #LED Signal
+const int PIN_TMP  = A0; // Temperature Sensor
+const int PIN_SOIL = A1; // Soil Sensor     
+const int PIN_LDR  = A2; // Light Sensor    
+const int PIN_POT  = A3; // Potentiometer     
+const int PIN_LED  = 13; // LED Signal
 
-int SOIL_DRY_RAW = 900; #Calibration for Wet Soil      
-int SOIL_WET_RAW = 350; #Calibration for Dry Soil          
+int SOIL_DRY_RAW = 900; // Calibration for Wet Soil      
+int SOIL_WET_RAW = 350; // Calibration for Dry Soil          
 
-#Setting up the range of accepted values
+// Setting up the range of accepted values
 const float T_OK_MIN=15.0, T_OK_MAX=30.0;
 const float SOIL_OK_MIN=30.0, SOIL_OK_MAX=70.0;
 const int N_SAMPLES=8; const unsigned PRINT_MS=500;
 
 int avgRead(uint8_t pin){ long s=0; for(int i=0;i<N_SAMPLES;i++){ s+=analogRead(pin); delay(2);} return s/N_SAMPLES; }
-#Converting the soil readings into moisture
+// Converting the soil readings into moisture
 float soilPctFromRaw(int raw){ 
   int dry=SOIL_DRY_RAW, wet=SOIL_WET_RAW;
   if(dry==wet) return 0;
@@ -44,7 +44,7 @@ void loop(){
 
   bool alert = !(tempC>=T_OK_MIN && tempC<=T_OK_MAX && soil>=SOIL_OK_MIN && soil<=SOIL_OK_MAX);
   digitalWrite(PIN_LED, alert ? HIGH : LOW); #Alerting users with LED if data is outside the range 
-
+// Print Value configured from the sensors
   Serial.print(ms);Serial.print(",");
   Serial.print(rTmp);Serial.print(",");
   Serial.print(rSoil);Serial.print(",");
