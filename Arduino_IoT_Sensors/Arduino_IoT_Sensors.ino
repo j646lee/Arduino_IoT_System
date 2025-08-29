16e100a716b3f223a1e2e95b093b3050b7a7cd44
Jiwon Lee <jiwonlee646@gmail.com>
2025-08-29 13:13:15 +0700

    Update Arduino_IoT_Sensors.ino

--- Arduino_IoT_Sensors/Arduino_IoT_Sensors.ino
+++ Arduino_IoT_Sensors/Arduino_IoT_Sensors.ino
@@ -1,20 +1,22 @@
 #define USE_POT_FOR_TEMP 0   
 
-const int PIN_TMP  = A0;
-const int PIN_SOIL = A1;     
-const int PIN_LDR  = A2;     
-const int PIN_POT  = A3;     
-const int PIN_LED  = 13;
+const int PIN_TMP  = A0; #Temperature Sensor
+const int PIN_SOIL = A1; #Soil Sensor     
+const int PIN_LDR  = A2; #Light Sensor    
+const int PIN_POT  = A3; #Potentiometer     
+const int PIN_LED  = 13; #LED Signal
 
-int SOIL_DRY_RAW = 900;      
-int SOIL_WET_RAW = 350;      
+int SOIL_DRY_RAW = 900; #Calibration for Wet Soil      
+int SOIL_WET_RAW = 350; #Calibration for Dry Soil          
 
+#Setting up the range of accepted values
 const float T_OK_MIN=15.0, T_OK_MAX=30.0;
 const float SOIL_OK_MIN=30.0, SOIL_OK_MAX=70.0;
 const int N_SAMPLES=8; const unsigned PRINT_MS=500;
 
 int avgRead(uint8_t pin){ long s=0; for(int i=0;i<N_SAMPLES;i++){ s+=analogRead(pin); delay(2);} return s/N_SAMPLES; }
-float soilPctFromRaw(int raw){
+#Converting the soil readings into moisture
+float soilPctFromRaw(int raw){ 
   int dry=SOIL_DRY_RAW, wet=SOIL_WET_RAW;
   if(dry==wet) return 0;
   raw = constrain(raw, min(dry,wet), max(dry,wet));
@@ -29,10 +31,10 @@ void setup(){
 }
 
 void loop(){
-  unsigned long ms=millis();
+  unsigned long ms=millis(); #Readings in miliseconds
 
-  int rSoil=avgRead(PIN_SOIL);
-  int rLdr =avgRead(PIN_LDR);
+  int rSoil=avgRead(PIN_SOIL); #Reading the raw value of the soil
+  int rLdr =avgRead(PIN_LDR);  #Reading the raw value of light
   int rTmp = USE_POT_FOR_TEMP ? avgRead(PIN_POT) : avgRead(PIN_TMP);
 
   float Vtmp  = rTmp*(5.0/1023.0);
@@ -41,7 +43,7 @@ void loop(){
   float ldr   = (rLdr/1023.0f)*100.0f;
 
   bool alert = !(tempC>=T_OK_MIN && tempC<=T_OK_MAX && soil>=SOIL_OK_MIN && soil<=SOIL_OK_MAX);
-  digitalWrite(PIN_LED, alert ? HIGH : LOW);
+  digitalWrite(PIN_LED, alert ? HIGH : LOW); #Alerting users with LED if data is outside the range 
 
   Serial.print(ms);Serial.print(",");
   Serial.print(rTmp);Serial.print(",");
